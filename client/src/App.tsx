ec9600b7e7211ca5c646b07892f6064b8cf7210e
Andrey Safonov <a.so777@live.ca>
2021-01-09 14:36:12 -0800

    Added Regions Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
+import React, { useEffect, useState } from 'react';
+import Regions from './components/Regions/Regions';
 import './App.css';
 
-const REGIONS = gql`
-query GetRegions {
-  regions {
-    name
-    country
-  }
-}
-`;
-
 const App = () => {
-  const { loading, error, data } = useQuery(REGIONS);
+  const [regionId, setRegionId] = useState<string>('');
+
+  useEffect(() => {
+    console.log(regionId);
+  }, [regionId]);
 
   return (
     <div className="App">
       <header className="App-header">
-        {loading ? <p>Loading...</p> : data.regions.map((region: any) => <div>{region.name}, {region.country}</div> )}
+        <Regions updateRegionId={setRegionId} />
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/Regions/Regions.tsx
@@ -0,0 +1,63 @@
+import React, { useState, Fragment } from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+interface Region {
+  id: string;
+  fullName: string;
+  name: string;
+  country: string;
+}
+
+interface RegionsResults {
+  regions: Region[];
+}
+
+interface RegionProps {
+    updateRegionId: (regionId: string) => void;
+}
+
+const REGIONS = gql`
+  query GetRegions {
+    regions {
+      id
+      fullName
+      name
+      country
+    }
+  }
+`;
+
+const Regions = (props: RegionProps) => {
+  const [regionId, setRegionId] = useState<string>('');
+
+  const { loading, data } = useQuery<RegionsResults>(REGIONS);
+
+  return (
+    <Fragment>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <p>
+          I want to see food trucks in{' '}
+          <select
+            value={regionId}
+            onChange={(e) => {
+              setRegionId(e.target.value);
+              props.updateRegionId(e.target.value);
+            }}
+          >
+            {data &&
+              data.regions &&
+              data.regions.map((region: Region) => (
+                <option value={region.id} key={region.id}>
+                  {region.fullName}
+                </option>
+              ))}
+          </select>
+        </p>
+      )}
+    </Fragment>
+  );
+};
+
+export default Regions;
